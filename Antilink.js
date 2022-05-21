a34b7ffe7393a7a6677282e3e2dbf79f1cc56318
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-05-21 13:12:36 +0300

    Create Antilink.js

---
+++ Antilink.js
@@ -0,0 +1,17 @@
+let handler = m => m
+
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isGroupLink = linkRegex.exec(m.text)
+
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*Group Link* Sent!. Leave before I kick 🥱`)
+   await this.groupRemove(m.chat, [m.sender])
+
+  }
+  return true
+}
+
+module.exports = handler
